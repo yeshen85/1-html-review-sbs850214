28f858e9774a18317a5ec2677b945fa577f085b6
sbs850214 <sbs850214@gmail.com>
2021-09-20 17:57:57 -0400

    Add API by viewjs

---
+++ app/public/js/index.js
@@ -0,0 +1,35 @@
+const Offer = {
+    data() {
+      return {
+        "person": {},
+        }
+    },
+    computed: {
+        prettyDate() {
+            return dayjs(this.person.dob.date)
+            .format('D MMM YYYY')
+        }
+    },
+    methods: {
+        fetchUserData(){
+            console.log("A");
+            fetch('https://randomuser.me/api/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                console.log("C");
+                this.person = responseJson.results[0];
+    
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+
+
+        }
+    },
+    created() {
+        this.fetchUserData();
+    } //end created
+} // end Offer config
+Vue.createApp(Offer).mount('#offerApp');
